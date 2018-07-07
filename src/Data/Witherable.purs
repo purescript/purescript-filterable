ba3fdeab96485127fc5ef8b0fb5f44857aab9b99
Gabe Johnson <gijohnson105@gmail.com>
2018-07-06 20:11:26 -0500

    Add more efficient Witherable instance for Array

--- src/Data/Witherable.purs
+++ src/Data/Witherable.purs
@@ -14,10 +14,14 @@ module Data.Witherable
 
 import Control.Applicative (class Applicative, (<*>), pure)
 import Control.Category ((<<<), identity)
+import Control.Monad.ST as ST
+import Data.Array ((!!))
+import Data.Array.ST as STA
+import Data.Array.ST.Iterator as STAI
 import Data.Compactable (compact, separate)
 import Data.Either (Either(..), either)
 import Data.Filterable (class Filterable)
-import Data.Functor ((<$>), map)
+import Data.Functor ((<$>), map, voidRight)
 import Data.Identity (Identity(..))
 import Data.List (List(..), (:))
 import Data.List as List
@@ -26,7 +30,7 @@ import Data.Maybe (Maybe(..), maybe)
 import Data.Monoid (class Monoid, mempty)
 import Data.Newtype (unwrap)
 import Data.Traversable (class Traversable, traverse)
-import Prelude (class Ord)
+import Prelude (class Ord, bind, const, discard, flip, unit, ($))
 
 -- | `Witherable` represents data structures which can be _partitioned_ with
 -- | effects in some `Applicative` functor.
@@ -98,8 +102,36 @@ withered :: forall t m x. Witherable t => Applicative m =>
 withered = wither identity
 
 instance witherableArray :: Witherable Array where
-  wilt = wiltDefault
-  wither = witherDefault
+  wilt p xs = ado
+    xs' <- traverse p xs
+    let left = ST.run (do
+         ls <- STA.empty
+         iter <- STAI.iterator (xs' !! _)
+         STAI.iterate iter
+           (voidRight unit <<< either (flip STA.push $ ls) (const $ pure 0))
+
+         STA.unsafeFreeze ls)
+
+    let right = ST.run (do
+         rs <- STA.empty
+         iter  <- STAI.iterator (xs' !! _)
+         STAI.iterate iter
+           (voidRight unit <<< either (const $ pure 0) (flip STA.push $ rs))
+
+         STA.unsafeFreeze rs)
+
+    in { left, right }
+
+  wither p xs = ado
+    xs' <- traverse p xs
+    in ST.run do
+      result <- STA.empty
+      iter <- STAI.iterator (xs' !! _)
+
+      STAI.iterate iter
+           (voidRight unit <<< maybe (pure 0) (flip STA.push $ result))
+
+      STA.unsafeFreeze result
 
 instance witherableList :: Witherable List where
   wilt p = map rev <<< List.foldl go (pure { left: Nil, right: Nil }) where
